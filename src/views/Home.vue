4359361bfe011960b8bd3bfc50e89c3c5dc05676
CrowdYellow <echo61546@gmail.com>
2019-07-05 16:23:51 +0800

    完善数据显示

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -28,8 +28,8 @@
                   <abbr class="timeago">{{ article.date | moment('from') }}</abbr>
                 </div>
               </router-link>
-              <router-link v-if="user" :to="`/${user.name}`" tag="div" class="avatar pull-left">
-                <img :src="user.avatar" class="media-object img-thumbnail avatar avatar-middle">
+              <router-link :to="`/${article.uname}`" tag="div" class="avatar pull-left">
+                <img :src="article.uavatar" class="media-object img-thumbnail avatar avatar-middle">
               </router-link>
               <router-link :to="`/articles/${article.articleId}/content`" tag="div" class="infos">
                 <div class="media-heading">
@@ -80,9 +80,11 @@
     computed: {
       ...mapState([
         'auth',
-        'user',
-        'articles'
-      ])
+        'user'
+      ]),
+      articles() {
+        return this.$store.getters.computedArticles
+      }
     },
     watch: {
       auth(value) {
