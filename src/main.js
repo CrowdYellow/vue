9536c12ad860f3bb4f1d99061a6f835611c4860f
CrowdYellow <echo61546@gmail.com>
2019-07-05 16:13:31 +0800

    安装 Mock.js

--- src/main.js
+++ src/main.js
@@ -7,10 +7,33 @@ import store from './store'
 import VueSweetalert2 from './plugins/vue-sweetalert2'
 import Message from './plugins/message'
 import './filters'
+import { mockArticles } from './mock/data'
+import ls from './utils/localStorage'
 
 Vue.config.productionTip = false
 Vue.use(VueSweetalert2)
 Vue.use(Message)
+
+const AddMockData = (() => {
+  // 是否加入测试数据
+  const isAddMockData = true
+  // 用户数据
+  let userArticles = ls.getItem('articles')
+
+  if (Array.isArray(userArticles)) {
+    userArticles = userArticles.filter(article => parseInt(article.uid) === 1)
+  } else {
+    userArticles = []
+  }
+
+  if (isAddMockData) {
+    // 合并用户数据和测试数据，使用合并值作为所有文章
+    store.commit('UPDATE_ARTICLES', [...userArticles, ...mockArticles(10)])
+  } else {
+    // 使用用户数据作为所有文章
+    store.commit('UPDATE_ARTICLES', userArticles)
+  }
+})()
 
 /* eslint-disable no-new */
 new Vue({
@@ -18,7 +41,4 @@ new Vue({
   router,
   store,
   render: h => h(App),
-  created() {
-    console.log(this.$options.el) // => '#app'
-  }
 })
