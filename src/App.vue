91dc13098eedbad465aea2213d0d547631c63224
CrowdYellow <echo61546@gmail.com>
2019-07-05 12:24:23 +0800

    安装scss

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+<template lang="pug">
+  div#app
+    img(src="./assets/logo.png")
+    h1 Welcome from pug & sass
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: 'App'
+  }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 200px;
+
+    h1 {
+      color: #42b983;
+    }
+  }
 </style>
